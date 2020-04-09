13e8453101903eafb629158b8551d64f76c51b0e
Marcos Castillo <marcoscastillo@correo.ugr.es>
2020-04-09 19:54:44 +0200

    Update #2 Ejercicio4
    Avances en el ejercicio 4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SG_UGR
 Prácticas de la signatura SG (Sistemas Gráficos) de 3er curso del Grado de Ingeniería Informática de la Universidad de Granada
+
+Se adjuntan los ejercicios, la primera parte de las prácticas de la asignatura, de JavaScript con ThreeJS.
--- ejercicio4/Objeto.js
+++ ejercicio4/Objeto.js
@@ -4,24 +4,172 @@ class Objeto extends THREE.Object3D{
         this.alpha = 0;
         this.createGUI(gui,titleGui);
 
+        this.createHeart();
+        this.createClover();
+        this.createDiamond();
+        this.createSpade();
+        this.createCloverExtruded();
+        this.createHeartExtruded()
+
+
+        this.add(this.meshHeart);
+        this.add(this.meshClover);
+        this.add(this.meshDiamond);
+        this.add(this.meshSpade);
+        this.add(this.cloverExtruded);
+        this.add(this.heartExtruded);
+    }
+
+    createDiamond(){
+        var diamondShape = new THREE.Shape();
+       
+        diamondShape.moveTo(10,0 );
+        diamondShape.lineTo(5,5);
+        diamondShape.lineTo(10,10);
+        diamondShape.lineTo(15,5);
+        diamondShape.lineTo(10,0);
+
+        var extrudeSettings = { amount: 2, bevelEnabled: true, bevelSegments: 1, steps: 1, bevelSize: 1, bevelThickness: 0.2 };
+
+        var geometry = new THREE.ExtrudeBufferGeometry( diamondShape, extrudeSettings );
+        var material = new THREE.MeshPhongMaterial({color: 0xFF0000});
+
+        this.meshDiamond = new THREE.Mesh( geometry, material );
+        this.meshDiamond.scale.set(0.1,0.1,0.1);
+        this.meshDiamond.position.set(1,2,0);
+        geometry.center();
+    }
+
+    createClover(){
+        var cloverShape = new THREE.Shape();
+       
+        cloverShape.moveTo(17,0 );
+        cloverShape.bezierCurveTo(17,0, 17.7,4, 17,5 );
+        cloverShape.bezierCurveTo(17,5, 11,1, 10,7 );
+        cloverShape.bezierCurveTo(10,7, 10.5,14, 15,8.5 );
+        cloverShape.bezierCurveTo(15,8.5, 12,13.5, 17.5,15 );
+        //MITAD 2
+        cloverShape.bezierCurveTo(17.5,15, 22,13.5, 19,8.5 );
+        cloverShape.bezierCurveTo(19,8.5, 23.5,14, 24,7 );
+        cloverShape.bezierCurveTo(24,7, 23,1, 18,5 );
+        cloverShape.bezierCurveTo(18,5, 17,4, 17.5,0 );
+
+        var extrudeSettings = { amount: 2, bevelEnabled: true, bevelSegments: 0, steps: 2, bevelSize: 1, bevelThickness: 1 };
+
+        var geometry = new THREE.ExtrudeBufferGeometry( cloverShape, extrudeSettings );
+        var material = new THREE.MeshPhongMaterial({color: 0x0000FF});
+
+        this.meshClover = new THREE.Mesh( geometry, material );
+        this.meshClover.scale.set(0.09,0.09,0.09);
+        this.meshClover.position.set(1,0,0);
+        geometry.center();
+    }
+
+    createHeart(){
         var heartShape = new THREE.Shape();
         heartShape.moveTo(25,25 );
-        heartShape.bezierCurveTo(25,25,20, 0, 0, 0 );
-        heartShape.bezierCurveTo(-30,0,-30,35,-30,35 );
-        heartShape.bezierCurveTo(-30,55,-10,77,25,95 );
-        heartShape.bezierCurveTo(60,77,80,55,80,35 );
-        heartShape.bezierCurveTo(80,35,80,0,50,0 );
-        heartShape.bezierCurveTo(35,0,25,25,25,25 );
+        heartShape.bezierCurveTo(25,25, 20,0, 0,0 );
+        heartShape.bezierCurveTo(-30,0, -30,35, -30,35 );
+        heartShape.bezierCurveTo(-30,55, -10,77, 25,95 );
+        heartShape.bezierCurveTo(60,77, 80,55, 80,35 );
+        heartShape.bezierCurveTo(80,35, 80,0, 50,0 );
+        heartShape.bezierCurveTo(35,0, 25,25, 25,25 );
 
         var extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
 
         var geometry = new THREE.ExtrudeBufferGeometry( heartShape, extrudeSettings );
+        var material = new THREE.MeshPhongMaterial({color: 0xFF0000});
 
-        this.meshHeart = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial() );
-        this.meshHeart.scale.set(0.01,0.01,0.01);
+        this.meshHeart = new THREE.Mesh( geometry, material );
+        this.meshHeart.scale.set(0.015,0.015,0.015);
         this.meshHeart.position.set(3,0,0);
+        this.meshHeart.rotation.set(3.1456,0,0,0);
+        geometry.center();
+    }
 
-        this.add(this.meshHeart);
+    createSpade(){
+        var spadeShape = new THREE.Shape();
+        spadeShape.moveTo(25,25 );
+        spadeShape.bezierCurveTo(25,25, 20,0, 0,0 );
+        spadeShape.bezierCurveTo(-30,0, -30,35, -30,35 );
+        spadeShape.bezierCurveTo(-30,55, -10,77, 25,95 );
+        spadeShape.bezierCurveTo(60,77, 80,55, 80,35 );
+        spadeShape.bezierCurveTo(80,35, 80,0, 50,0 );
+        spadeShape.bezierCurveTo(35,0, 25,25, 25,25 );
+
+        var extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
+
+        var geometry = new THREE.ExtrudeBufferGeometry( spadeShape, extrudeSettings );
+        var material = new THREE.MeshPhongMaterial({color: 0x0000FF});
+
+        this.meshSpade = new THREE.Mesh( geometry, material );
+        this.meshSpade.scale.set(0.015,0.015,0.015);
+        this.meshSpade.position.set(3,2,0);
+        geometry.center();
+    }
+
+    createCloverExtruded(){
+        var curve = new THREE.CatmullRomCurve3( [
+            new THREE.Vector3( - 100, - 100, 0 ),
+            new THREE.Vector3( - 50, 0, -50 ),
+            new THREE.Vector3( 0, 100, 100 ),
+            new THREE.Vector3( 50, 150, -30 ),
+            new THREE.Vector3( 100, 200, 0 )
+        ] );
+
+        var cloverShape = new THREE.Shape();
+       
+        cloverShape.moveTo(17,0 );
+        cloverShape.bezierCurveTo(17,0, 17.7,4, 17,5 );
+        cloverShape.bezierCurveTo(17,5, 11,1, 10,7 );
+        cloverShape.bezierCurveTo(10,7, 10.5,14, 15,8.5 );
+        cloverShape.bezierCurveTo(15,8.5, 12,13.5, 17.5,15 );
+        //MITAD 2
+        cloverShape.bezierCurveTo(17.5,15, 22,13.5, 19,8.5 );
+        cloverShape.bezierCurveTo(19,8.5, 23.5,14, 24,7 );
+        cloverShape.bezierCurveTo(24,7, 23,1, 18,5 );
+        cloverShape.bezierCurveTo(18,5, 17,4, 17.5,0 );
+
+        var extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1, extrudePath: curve };
+
+        var geometry = new THREE.ExtrudeBufferGeometry( cloverShape, extrudeSettings );
+        var material = new THREE.MeshPhongMaterial({color: 0x00FF00});
+
+        this.cloverExtruded = new THREE.Mesh( geometry, material );
+        this.cloverExtruded.scale.set(0.02,0.02,0.02);
+        this.cloverExtruded.position.set(-1,0.5,0);
+        this.cloverExtruded.rotation.z+=0.8;
+        geometry.center();
+    }
+
+    createHeartExtruded(){
+        var curve = new THREE.CatmullRomCurve3( [
+            new THREE.Vector3( - 100, - 100, 0 ),
+            new THREE.Vector3( - 50, 0, -50 ),
+            new THREE.Vector3( 0, 100, 100 ),
+            new THREE.Vector3( 50, 150, -30 ),
+            new THREE.Vector3( 100, 200, 0 )
+        ] );
+
+        var heartShape = new THREE.Shape();
+        heartShape.moveTo(25,25 );
+        heartShape.bezierCurveTo(25,25, 20,0, 0,0 );
+        heartShape.bezierCurveTo(-30,0, -30,35, -30,35 );
+        heartShape.bezierCurveTo(-30,55, -10,77, 25,95 );
+        heartShape.bezierCurveTo(60,77, 80,55, 80,35 );
+        heartShape.bezierCurveTo(80,35, 80,0, 50,0 );
+        heartShape.bezierCurveTo(35,0, 25,25, 25,25 );
+
+        var extrudeSettings = { amount: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1, extrudePath: curve };
+
+        var geometry = new THREE.ExtrudeBufferGeometry( heartShape, extrudeSettings );
+        var material = new THREE.MeshPhongMaterial({color: 0x00FF00});
+
+        this.heartExtruded = new THREE.Mesh( geometry, material );
+        this.heartExtruded.scale.set(0.01,0.01,0.01);
+        this.heartExtruded.position.set(5,0.5,0);
+        this.heartExtruded.rotation.z+=0.8;
+        geometry.center();
     }
 
     createGUI(gui,titleGui){
@@ -38,7 +186,12 @@ class Objeto extends THREE.Object3D{
     update(){
 
         if(this.animacion){
-            this.meshHeart.rotation.y+=0.01;
+            this.meshHeart.rotation.y-=0.01;
+            this.meshClover.rotation.y+=0.01;
+            this.meshDiamond.rotation.y+=0.01;
+            this.meshSpade.rotation.y+=0.01;
+            this.heartExtruded.rotation.y+=0.01;
+            this.cloverExtruded.rotation.y+=0.01;
             //this.meshHeart.rotation.z+=0.01;
         }
         
